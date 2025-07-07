a1f785a87ff459851734592dfc84c37b75b56264
Kholil Abdi Prasetiyo <kholilapras@gmail.com>
2025-07-07 22:14:51 +0700

    add photo project 4

--- config/project-data.ts
+++ config/project-data.ts
@@ -125,8 +125,8 @@ const projectConfigs: Array<{
     {
       base: {
         images: [
-          "/p4/",
-          "/p4/",
+          "/p4/1.png",
+          "/p4/2.png",
         ],
         technologies: ["HTML", "Bootstrap", "JavaScript (Vanilla)"],
         viewCodeUrl: "https://github.com/kholilapras/REPAKINET",
